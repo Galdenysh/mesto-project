71a7b8584476bcc25f437b26e1dd6d0f570f805b
Igor <91311311+Galdenysh@users.noreply.github.com>
2021-11-09 20:33:32 +0300

    add popup add-place

--- index.js
+++ index.js
@@ -3,25 +3,20 @@ const profileBlock = document.querySelector(".profile");
 let userName = profileBlock.querySelector(".profile__name");
 let userSignature = profileBlock.querySelector(".profile__signature");
 
-const popupBlock = document.querySelector(".popup"); // Находим попап
+const popupEditBlock = document.querySelector("#js-popup-edit"); // Находим попап пользователя
 
 // Находим форму и поля формы в DOM
 const formElement = document.querySelector(".popup__form");
 const nameInput = formElement.querySelector(".popup__form-item_select_name");
 const signatureInput = formElement.querySelector(".popup__form-item_select_signature");
 
-// Функция открытия попапа
-function openPopup() {
-  // Ниже заполнение полей форм из информации профиля
-  nameInput.value = userName.textContent;
-  signatureInput.value = userSignature.textContent;
+// Ниже заполнение полей форм из информации профиля
+nameInput.value = userName.textContent;
+signatureInput.value = userSignature.textContent;
 
-  popupBlock.classList.add("popup_opened"); // Открытие попапа
-}
-
-// Функция закрытия попапа
-function closePopup() {
-  popupBlock.classList.remove("popup_opened");
+// Функция открытия попапа редактирования пользователя
+function openPopupEdit() {
+  popupEditBlock.classList.add("popup_opened");
 }
 
 // Обработчик «отправки» формы
@@ -32,7 +27,24 @@ function formSubmitHandler (evt) {
     userName.textContent = nameInput.value;
     userSignature.textContent = signatureInput.value;
 
-    closePopup();
+    closePopupEdit();
+}
+
+// Функция закрытия попапа редактирования пользователя
+function closePopupEdit() {
+  popupEditBlock.classList.remove("popup_opened");
+}
+
+const popupAddBlock = document.querySelector("#js-popup-add"); // Находим попап добавления места
+
+// Функция открытия попапа добавления места
+function openPopupAdd() {
+  popupAddBlock.classList.add("popup_opened");
+}
+
+// Функция закрытия попапа добавления места
+function closePopupAdd() {
+  popupAddBlock.classList.remove("popup_opened");
 }
 
 const cardsBlock = document.querySelector(".cards");
@@ -43,14 +55,18 @@ function likeToggle() {
 }
 
 // Находим кнопки
-const openPopupBtn = profileBlock.querySelector(".profile__edit-button");
-const closePopupBtn = popupBlock.querySelector(".popup__close-button");
+const openPopupEditBtn = profileBlock.querySelector(".profile__edit-button");
+const openPopupAddBtn = profileBlock.querySelector(".profile__add-button");
+const closePopupEditBtn = popupEditBlock.querySelector(".popup__close-button");
+const closePopupAddBtn = popupAddBlock.querySelector(".popup__close-button")
 const cardsLikeBtn = cardsBlock.querySelector(".cards__like-button");
 
 // Слушаем кнопки
 cardsLikeBtn.addEventListener("click", likeToggle);
-openPopupBtn.addEventListener("click", openPopup);
-closePopupBtn.addEventListener("click", closePopup);
+openPopupEditBtn.addEventListener("click", openPopupEdit);
+openPopupAddBtn.addEventListener("click", openPopupAdd);
+closePopupEditBtn.addEventListener("click", closePopupEdit);
+closePopupAddBtn.addEventListener("click", closePopupAdd);
 
 // Слушаем отправку формы
 formElement.addEventListener('submit', formSubmitHandler);
