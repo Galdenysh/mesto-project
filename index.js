e698a9a8a637b2f7ede24f700db1d9c1bdea36f3
Igor <91311311+Galdenysh@users.noreply.github.com>
2021-11-10 12:44:05 +0300

    add features add_card and like

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 // Получение информации профиля
 const profileBlock = document.querySelector(".profile");
-let userName = profileBlock.querySelector(".profile__name");
-let userSignature = profileBlock.querySelector(".profile__signature");
+const userName = profileBlock.querySelector(".profile__name");
+const userSignature = profileBlock.querySelector(".profile__signature");
 
+// Находим форму и поля формы пользователя
 const popupEditBlock = document.querySelector("#js-popup-edit"); // Находим попап пользователя
-
-// Находим форму и поля формы в DOM
-const formElement = document.querySelector(".popup__form");
-const nameInput = formElement.querySelector(".popup__form-item_select_name");
-const signatureInput = formElement.querySelector(".popup__form-item_select_signature");
-
-// Ниже заполнение полей форм из информации профиля
-nameInput.value = userName.textContent;
-signatureInput.value = userSignature.textContent;
+const formEditElement = popupEditBlock.querySelector(".popup__form");
+const nameInput = formEditElement.querySelector(".popup__form-item_select_name");
+const signatureInput = formEditElement.querySelector(".popup__form-item_select_signature");
 
 // Функция открытия попапа редактирования пользователя
 function openPopupEdit() {
   popupEditBlock.classList.add("popup_opened");
+
+  // Ниже заполнение полей форм из информации профиля
+  nameInput.value = userName.textContent;
+  signatureInput.value = userSignature.textContent;
 }
 
-// Обработчик «отправки» формы
-function formSubmitHandler (evt) {
+// Функция закрытия попапа редактирования пользователя
+function closePopupEdit() {
+  popupEditBlock.classList.remove("popup_opened");
+}
+
+// Обработчик «отправки» формы данных пользователя
+function formEditSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы
 
     // Записываем в профиль значение из полей форм
@@ -30,16 +34,19 @@ function formSubmitHandler (evt) {
     closePopupEdit();
 }
 
-// Функция закрытия попапа редактирования пользователя
-function closePopupEdit() {
-  popupEditBlock.classList.remove("popup_opened");
-}
-
+// Находим форму добавления места
 const popupAddBlock = document.querySelector("#js-popup-add"); // Находим попап добавления места
+const formAddElement = popupAddBlock.querySelector(".popup__form");
+const placeNameInput = formAddElement.querySelector(".popup__form-item_select_place-name");
+const pictureUrlInput = formAddElement.querySelector(".popup__form-item_select_picture-url");
 
 // Функция открытия попапа добавления места
 function openPopupAdd() {
   popupAddBlock.classList.add("popup_opened");
+
+  // Ниже опусташаем поля формы
+  placeNameInput.value = "";
+  pictureUrlInput.value = "";
 }
 
 // Функция закрытия попапа добавления места
@@ -47,11 +54,34 @@ function closePopupAdd() {
   popupAddBlock.classList.remove("popup_opened");
 }
 
-const cardsBlock = document.querySelector(".cards");
+// Обработчик «отправки» формы добавления места
+function formAddSubmitHandler (evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы
 
-function likeToggle() {
-  let cardsLike = cardsBlock.querySelector(".cards__like-button");
-  cardsLike.classList.toggle("cards__like-button_active");
+  // Записываем в переменные значение из полей форм
+  const placeName = placeNameInput.value;
+  const pictureUrl = pictureUrlInput.value;
+
+  closePopupAdd();
+  addCard(placeName, pictureUrl); // вызываем функцию добавления карточки и посылаем значения форм
+}
+
+// Функция добавления карточки
+function addCard(placeName, pictureUrl) {
+  const cardTemplate = document.querySelector("#js-cards").content;
+  const cardElement = cardTemplate.querySelector(".cards__card").cloneNode(true);
+
+  // Заполняем карточку
+  cardElement.querySelector(".cards__title").textContent = placeName;
+  cardElement.querySelector(".cards__place").src = pictureUrl;
+  cardElement.querySelector(".cards__place").alt = placeName;
+
+  // Реализация лайка
+  cardElement.querySelector('.cards__like-button').addEventListener('click', function(evt) {
+    evt.target.classList.toggle("cards__like-button_active");
+  });
+
+  document.querySelector(".cards").append(cardElement); // Пушим карточку
 }
 
 // Находим кнопки
@@ -59,14 +89,13 @@ const openPopupEditBtn = profileBlock.querySelector(".profile__edit-button");
 const openPopupAddBtn = profileBlock.querySelector(".profile__add-button");
 const closePopupEditBtn = popupEditBlock.querySelector(".popup__close-button");
 const closePopupAddBtn = popupAddBlock.querySelector(".popup__close-button")
-const cardsLikeBtn = cardsBlock.querySelector(".cards__like-button");
 
 // Слушаем кнопки
-cardsLikeBtn.addEventListener("click", likeToggle);
 openPopupEditBtn.addEventListener("click", openPopupEdit);
 openPopupAddBtn.addEventListener("click", openPopupAdd);
 closePopupEditBtn.addEventListener("click", closePopupEdit);
 closePopupAddBtn.addEventListener("click", closePopupAdd);
 
 // Слушаем отправку формы
-formElement.addEventListener('submit', formSubmitHandler);
+formEditElement.addEventListener("submit", formEditSubmitHandler);
+formAddElement.addEventListener("submit", formAddSubmitHandler);
